5befcf6f913566e75d51b2c8d145c0ab7869c577
PatoFlamejanteTV <flamejantepato77@gmail.com>
2025-01-18 12:40:03 -0300

    base of the base of the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the baseof the base

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PMod")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PMod")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d795822b-7ae1-4267-bc68-af5e3d757bb5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ PMOD_MAIN.vb
@@ -0,0 +1,38 @@
+﻿Imports LuaInterface
+
+Public Class PMOD_MAIN
+    Private Sub PMOD_ENGINE_STARTUP(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim lua As New Lua()
+
+        ' DEFINE: Variables
+        lua.DoString("_P = {}") ' Defines '_P' table, like the '_G' one, but in this case for read-only PMod things.
+
+        lua("_P.VERSION") = "0.0.1"
+        lua("_P.VERSIONSTATE") = "Concept"
+
+        ' DEFINE: Functions
+        lua.RegisterFunction("WinForms_MessageBox", Me, [GetType]().GetMethod("WinForms_MessageBox"))
+        lua.RegisterFunction("WinForms_CloseMainForm", Me, [GetType]().GetMethod("WinForms_CloseMainForm"))
+
+        lua.RegisterFunction("System_Command", Me, [GetType]().GetMethod("System_Command"))
+
+    End Sub
+
+    Public Function WinForms_MessageBox(msg As String)
+        Return MessageBox.Show(msg)
+    End Function
+    Public Function WinForms_CloseMainForm()
+        Application.Exit()
+        Return "defined"
+    End Function
+    Public Function System_Command(command As String, args As String, permanent As Boolean)
+        Dim p As Process = New Process()
+        Dim pi As ProcessStartInfo = New ProcessStartInfo()
+        pi.Arguments = " " + If(permanent = True, "/K", "/C") + " " + command + " " + args
+        pi.FileName = "cmd.exe"
+        p.StartInfo = pi
+        p.Start()
+        Return {p, pi} 'return both Process and ProcessStartInfo
+    End Function
+
+End Class
