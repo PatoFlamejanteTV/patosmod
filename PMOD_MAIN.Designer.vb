f01c7e9fafed470b4344ac8763d43e2c0e19ec53
PatoFlamejanteTV <flamejantepato77@gmail.com>
2025-01-18 12:49:22 -0300

    Added an mods/lua view button, along with the whole filesystem *.lua recognizing system.

--- PMOD_MAIN.Designer.vb
+++ PMOD_MAIN.Designer.vb
@@ -22,16 +22,29 @@ Partial Class PMOD_MAIN
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ButtonOpenFolder = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'ButtonOpenFolder
+        '
+        Me.ButtonOpenFolder.Location = New System.Drawing.Point(13, 13)
+        Me.ButtonOpenFolder.Name = "ButtonOpenFolder"
+        Me.ButtonOpenFolder.Size = New System.Drawing.Size(272, 23)
+        Me.ButtonOpenFolder.TabIndex = 0
+        Me.ButtonOpenFolder.Text = "ButtonOpenFolder"
+        Me.ButtonOpenFolder.UseVisualStyleBackColor = True
         '
         'PMOD_MAIN
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(297, 297)
+        Me.Controls.Add(Me.ButtonOpenFolder)
         Me.Name = "PMOD_MAIN"
         Me.Text = "PMOD_MAIN"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents ButtonOpenFolder As Button
 End Class
--- PMOD_MAIN.vb
+++ PMOD_MAIN.vb
@@ -1,14 +1,38 @@
 ﻿Imports LuaInterface
+Imports System.IO
+
 
 Public Class PMOD_MAIN
     Private Sub PMOD_ENGINE_STARTUP(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim lua As New Lua()
+        Dim modsPath As String = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "mods", "lua")
+
+        ' Check if the directory exists, if not, create it
+        If Not Directory.Exists(modsPath) Then
+            Directory.CreateDirectory(modsPath)
+            Console.WriteLine("The directory 'mods/lua' was not found and has been created.")
+            Return
+        End If
+
+        ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+        Dim lua As New Lua() ' most important line /gen
+
+        Dim luaFiles As String() = Directory.GetFiles(modsPath, "*.lua")
+
+        For Each luaFile In luaFiles
+            Try
+                lua.DoFile(luaFile)
+                Console.WriteLine($"Executed script: {Path.GetFileName(luaFile)}")
+            Catch ex As Exception
+                Console.WriteLine($"Error executing script {Path.GetFileName(luaFile)}: {ex.Message}")
+            End Try
+        Next
 
         ' DEFINE: Variables
         lua.DoString("_P = {}") ' Defines '_P' table, like the '_G' one, but in this case for read-only PMod things.
 
         lua("_P.VERSION") = "0.0.1"
-        lua("_P.VERSIONSTATE") = "Concept"
+        lua("_P.VERSIONSTATE") = "Concept" 'Concept -> Alpha -> Beta -> Pre-Final -> Final
 
         ' DEFINE: Functions
         lua.RegisterFunction("WinForms_MessageBox", Me, [GetType]().GetMethod("WinForms_MessageBox"))
@@ -22,8 +46,9 @@ Public Class PMOD_MAIN
         Return MessageBox.Show(msg)
     End Function
     Public Function WinForms_CloseMainForm()
+        Return "closing" ' in case IO/System errors
         Application.Exit()
-        Return "defined"
+        Return "closed"
     End Function
     Public Function System_Command(command As String, args As String, permanent As Boolean)
         Dim p As Process = New Process()
@@ -34,5 +59,10 @@ Public Class PMOD_MAIN
         p.Start()
         Return {p, pi} 'return both Process and ProcessStartInfo
     End Function
+
+    Private Sub ButtonOpenFolder_Click(sender As Object, e As EventArgs) Handles ButtonOpenFolder.Click
+        Dim modsPath As String = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "mods", "lua")
+        Process.Start("explorer.exe", modsPath)
+    End Sub
 
 End Class
